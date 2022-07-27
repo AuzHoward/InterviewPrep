4de9a65668123899aff367256a21b85fb66fd33b
Auz <austinleehoward@gmail.com>
2022-07-26 22:26:16 -0500

    update roundtrip radio

--- src/test/java/Element.java
+++ src/test/java/Element.java
@@ -3,7 +3,7 @@ public abstract class Element {
     protected String southwestURL = "https://southwest.com";
 
     //Round Trip and One-way Radio buttons
-    protected String roundTripRadio = "#TabbedArea_4-panel-0 > div > div > div > form > div.search-form--options > div.search-form--options-trip > fieldset > ul > li:nth-child(1) > label > input";
+    protected String roundTripRadio = ".search-form--options-trip > div:nth-child(1) > fieldset:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > label:nth-child(1) > input:nth-child(1)";
     protected String oneWayRadio = "#TabbedArea_4-panel-0 > div > div > div > form > div.search-form--options > div.search-form--options-trip > fieldset > ul > li:nth-child(2) > label > input";
 
     //From and To Airport Fields
@@ -11,4 +11,6 @@ public abstract class Element {
     protected String toAirportField = "#LandingAirBookingSearchForm_destinationAirportCode";
 
     protected String searchFlightButton = "#LandingAirBookingSearchForm_submit-button";
+
+
 }
